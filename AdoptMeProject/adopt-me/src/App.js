ffa49f20d889a7d89d2227b6cddf406a4365e3c1
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-30 15:58:25 +0200

    Add AuthProvider

--- AdoptMeProject/adopt-me/src/App.js
+++ AdoptMeProject/adopt-me/src/App.js
@@ -2,27 +2,24 @@ import { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import { petServiceFactory } from './services/petService';
-import { authServiceFactory } from './services/authService';
-import { AuthContext } from './contexts/AuthContext';
-import { useService } from './hooks/useService';
+import { AuthProvider } from './contexts/AuthContext';
 
-import { CreatePet } from './components/CreatePet/CreatePet';
-import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login"
 import { Register } from "./components/Register/Register"
-import { Catalog } from "./components/Catalog/Catalog"
-import { PetDetails } from './components/PetDetails/PetDetails';
 import { Logout } from './components/Logout/Logout';
+import { Catalog } from "./components/Catalog/Catalog"
+import { CreatePet } from './components/CreatePet/CreatePet';
 import { EditPet } from './components/EditPet/EditPet';
+import { PetDetails } from './components/PetDetails/PetDetails';
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
     const navigate = useNavigate();
     const [pets, setPets] = useState([]);
-    const [auth, setAuth] = useState({});
-    const petService = petServiceFactory(auth.accessToken);
-    const authService = authServiceFactory(auth.accessToken);
+    const petService = petServiceFactory(); //auth.accessToken
+
 
     useEffect(() => {
         petService.getAll()
@@ -37,39 +34,6 @@ function App() {
         navigate('/catalog');
     }
 
-    const onLoginSubmit = async (data) => {
-        try {
-            const result = await authService.login(data);
-            setAuth(result);
-
-            navigate('/catalog');
-        } catch (error) {
-            console.log('Problem');
-        }
-
-    };
-
-    const onRegisterSubmit = async (values) => {
-        const { confirmPassword, ...registerData } = values;
-        if (confirmPassword !== registerData.password) {
-            return;
-        };
-
-        try {
-            const result = await authService.register(registerData);
-            setAuth(result);
-
-            navigate('/catalog');
-        } catch (error) {
-            console.log('Problem');
-        }
-
-    };
-
-    const onLogout = async (data) => {
-        await authService.logout();
-        setAuth({});
-    };
 
     const onPetEditSubmit = async (values) => {
         const result = await petService.edit(values._id, values);
@@ -80,18 +44,10 @@ function App() {
     };
 
 
-    const context = {
-        onLoginSubmit,
-        onRegisterSubmit,
-        onLogout,
-        userId: auth._id,
-        token: auth.accessToken,
-        userEmail: auth.email,
-        isAuthenticated: !!auth.accessToken,
-    };
+    
 
     return (
-        <AuthContext.Provider value={context}>
+        <AuthProvider>
             <div id="box">
 
                 <Header />
@@ -110,7 +66,7 @@ function App() {
                 </main>
                 <Footer />
             </div>
-        </AuthContext.Provider>
+        </AuthProvider>
     );
 }
 
