eb11328555692a522d914102ce9978d73b4e7cc9
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-30 16:13:21 +0200

    Add useAuthContext hook

--- AdoptMeProject/adopt-me/src/contexts/AuthContext.js
+++ AdoptMeProject/adopt-me/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -66,4 +66,10 @@ export const AuthProvider = ({
             </AuthContext.Provider>
         </>
     );
+};
+
+export const useAuthContext = () =>{
+    const context = useContext(AuthContext);
+
+    return context;
 };
