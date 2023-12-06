e85107d3e925efc354506b81e975818062aeb042
Dani Simeonova <danisimeonova147@gmail.com>
2023-12-06 14:56:41 +0200

    Add firebase config

--- AdoptMeProject/adopt-me/src/components/Catalog/CatalogItem/CatalogItem.test.js
+++ AdoptMeProject/adopt-me/src/components/Catalog/CatalogItem/CatalogItem.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { CatalogItem } from './CatalogItem';
 
 describe('CatalogItem', () => {
--- AdoptMeProject/adopt-me/src/services/petService.js
+++ AdoptMeProject/adopt-me/src/services/petService.js
@@ -1,33 +1,36 @@
 import { requestFactory } from './requester';
 
-const baseUrl = 'http://localhost:3030/data/pets';
+const baseUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3030'
+    : '';  // TODO: Add server url when deployed
+const url = `${baseUrl}/data/pets`;
 
 export const petServiceFactory = (token) => {
     const request = requestFactory(token);
 
     const getAll = async () => {
-        const result = await request.get(baseUrl);
+        const result = await request.get(url);
         const pets = Object.values(result);
 
         return pets;
     };
 
     const getOne = async (petId) => {
-        const result = await request.get(`${baseUrl}/${petId}`);
+        const result = await request.get(`${url}/${petId}`);
         console.log(result);
 
         return result;
     };
 
     const create = async (petData) => {
-        const result = await request.post(baseUrl, petData);
+        const result = await request.post(url, petData);
 
         return result;
     };
 
-    const deletePet = (petId) => request.delete(`${baseUrl}/${petId}`);
+    const deletePet = (petId) => request.delete(`${url}/${petId}`);
 
-    const edit = (petId, data) => request.put(`${baseUrl}/${petId}`, data);
+    const edit = (petId, data) => request.put(`${url}/${petId}`, data);
 
     return {
         getAll,
