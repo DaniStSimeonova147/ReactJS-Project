eabadc04252f1edbbb6457241594beaa2376b937
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-10 15:53:02 +0300

    feat: add MaterialUI to the Login component

--- AdoptMeProject/adopt-me/src/components/Login/Login.js
+++ AdoptMeProject/adopt-me/src/components/Login/Login.js
@@ -1,61 +1,72 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 
+import { Grid, TextField, Typography, Button } from '@mui/material';
+
 import { AuthContext } from '../../contexts/AuthContext';
 import { loginSchema } from './validations';
+import { styles } from './styles';
 
 const initialValues = {
   email: '',
   password: ''
 }
 
+const ErrorHandling = ({ field, form: { touched, errors }, ...props }) => (
+  <TextField
+    {...field}
+    {...props}
+    error={touched[field.name] && !!errors[field.name]}
+    helperText={touched[field.name] && errors[field.name]}
+    fullWidth
+    margin='dense'
+  />
+);
 export const Login = () => {
   const { onLoginSubmit } = useContext(AuthContext);
 
   return (
-    <section id="login-page" className="auth">
-      <div className="login-register-div">
-        <div className="login-register-box">
-          <h1>Adopt ME</h1>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={loginSchema}
-            onSubmit={(values) => {
-              onLoginSubmit(values);
-            }}
-          >
-            {() => (
-              <Form id="login" >
-                <p>
-                  <label>Email:</label>
-                  <Field
-                    name='email'
-                    type="email"
-                    id="email"
-                    placeholder="Email"
-                  />
-                  <ErrorMessage name="email" component="span" style={{ color: 'red' }} />
-                </p>
-                <p>
-                  <label>Password:</label>
-                  <Field
-                    name='password'
-                    type="password"
-                    id="login-password"
-                    placeholder="Password"
-                  />
-                  <ErrorMessage name="password" component="span" style={{ color: 'red' }} />
-                </p>
-                <button type="submit">LogIn</button>
-              </Form>
-            )}
-          </Formik>
-          <p>
-            Don't have account? <Link to="/register">Register</Link>
-          </p>
-        </div>
-      </div>
-    </section>
+    <Grid style={styles.form}>
+      <Typography style={styles.header} variant='h1' >
+        Adopt ME
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          onLoginSubmit(values);
+        }}
+      >
+        {() => (
+          <Form  >
+            <Field
+              name='email'
+              type='email'
+              label='Email'
+              component={ErrorHandling}
+            />
+            <Field
+              name='password'
+              type='password'
+              label='Password'
+              component={ErrorHandling}
+            />
+            <Button
+              type='submit'
+              variant='contained'
+              color='info'
+              fullWidth
+              sx={styles.button}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <Typography variant='overline'>
+        Don't have account? <Link to='/register'>Register</Link>
+      </Typography>
+    </Grid>
   );
 };
---
+++ AdoptMeProject/adopt-me/src/components/Login/styles.js
@@ -0,0 +1,25 @@
+export const styles = {
+    header: {
+        fontFamily: "Indie Flower",
+        fontSize: '40px',
+        textAlign: 'center',
+        fontWeight: 900
+    },
+    form: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: 800, 
+        padding: '30px 40px',
+        margin: '200px 50px 100px 50px ',
+        border: '1px solid grey',
+        borderRadius: '30px',
+        boxShadow: '2px 2px 3px #2565ae'
+    },
+    button: {
+        margin:'10px 0 10px 0',
+        backgroundColor: 'black',
+
+    }
+};
+
