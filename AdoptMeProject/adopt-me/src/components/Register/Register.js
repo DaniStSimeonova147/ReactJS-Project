59f2f5335b5a694b84d657f22cd95b97944dcbc4
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-11 10:51:25 +0300

    feat: add MaterialUI to the Register component

--- AdoptMeProject/adopt-me/src/components/Register/Register.js
+++ AdoptMeProject/adopt-me/src/components/Register/Register.js
@@ -1,9 +1,12 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 
+import { Grid, TextField, Typography, Button } from '@mui/material';
+
 import { AuthContext } from '../../contexts/AuthContext';
 import { registerSchema } from './validations';
+import { styles } from '../Login/styles';
 
 const initialValues = {
     email: '',
@@ -11,56 +14,68 @@ const initialValues = {
     confirmPassword: ''
 }
 
+const ErrorHandling = ({ field, form: { touched, errors }, ...props }) => (
+    <TextField 
+        {...field}
+        {...props}
+        error={touched[field.name] && !!errors[field.name]}
+        helperText={touched[field.name] && errors[field.name]}
+        fullWidth
+        margin='dense'
+    />
+);
+
 export const Register = () => {
     const { onRegisterSubmit } = useContext(AuthContext);
 
     return (
-        <section id="register-page" className="content auth">
-            <div className="login-register-div">
-                <div className="login-register-box">
-                    <h1>Adopt ME</h1>
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={registerSchema}
-                        onSubmit={(values) => {
-                            onRegisterSubmit(values);
-                        }}
-                    >
-                        {() => (
-                            <Form id="register">
-                                <label htmlFor="email">Email:</label>
-                                <Field
-                                    name="email"
-                                    type="email"
-                                    id="email"
-                                    placeholder="Email"
-                                />
-                                <ErrorMessage name="email" component="span" style={{ color: 'red' }} />
-                                <label htmlFor="pass">Password:</label>
-                                <Field
-                                    name="password"
-                                    type="password"
-                                    id="register-password"
-                                    placeholder="Password"
-                                />
-                                <ErrorMessage name="password" component="span" style={{ color: 'red' }} />
-                                <label htmlFor="con-pass">Confirm Password:</label>
-                                <Field
-                                    name="confirmPassword"
-                                    type="password"
-                                    id="confirm-password"
-                                    placeholder="Repeat Password"
-                                />
-                                <ErrorMessage name="confirmPassword" component="span" style={{ color: 'red' }} />
-                                <button type="submit">Register</button>
-                            </Form>
-                        )}
-                    </Formik>
-                    <p>
-                        Have an account? <Link to="/login">LogIn</Link>
-                    </p>
-                </div>
-            </div>
-        </section>
+        <Grid style={styles.form} >
+            <Typography style={styles.header} variant='h1' >
+                Adopt ME
+            </Typography>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={registerSchema}
+                onSubmit={(values) => {
+                    onRegisterSubmit(values);
+                }}
+            >
+                {() => (
+                    <Form >
+                        <Field
+                            name="email"
+                            type="email"
+                            label="Email"
+                            component={ErrorHandling}
+                        />
+                        <Field
+                            name="password"
+                            type="password"
+                            label="Password"
+                            component={ErrorHandling}
+                        />
+                        <Field
+                            name="confirmPassword"
+                            type="password"
+                            label="Repeat Password"
+                            component={ErrorHandling}
+                        />
+                        <Button
+                            type="submit"
+                            variant='contained'
+                            color='info'
+                            fullWidth
+                            sx={styles.button}
+                        >
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+            <Typography variant='overline'>
+                Have an account? <Link to="/login">LogIn</Link>
+            </Typography>
+        </Grid>
+        
     );
-}
+};
