e55d4b2ad5e84955597576647a5f9d96ff6250be
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-26 18:28:50 +0300

    refactor: refactor Login component, styles and validations

--- AdoptMeProject/adopt-me/src/components/Login/styles.js
+++ AdoptMeProject/adopt-me/src/components/Login/styles.js
@@ -1,25 +1,21 @@
 export const styles = {
     header: {
-        fontFamily: "Indie Flower",
+        fontFamily: 'cursive',
         fontSize: '30px',
         textAlign: 'center',
         fontWeight: 900
     },
     form: {
-        display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
-        width: 400, 
         padding: '20px 30px',
-        margin: '200px 50px 100px 50px ',
         border: '1px solid grey',
         borderRadius: '30px',
-        boxShadow: '2px 2px 3px #2565ae'
+        boxShadow: '1px 1px 1px grey',
     },
     button: {
-        margin:'10px 0 10px 0',
+        marginTop: '20px',
         backgroundColor: 'black',
-
     }
 };
 
--- AdoptMeProject/adopt-me/src/components/Login/validations.js
+++ AdoptMeProject/adopt-me/src/components/Login/validations.js
@@ -1,9 +1,22 @@
 import * as Yup from 'yup';
+import { TextField } from '@mui/material';
 
 export const loginSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Invalid email format!')
-      .required('Email is required!'),
-    password: Yup.string()
-      .required('Password is required!')
-  });
+  email: Yup.string()
+    .email('Invalid email format!')
+    .required('Email is required!'),
+  password: Yup.string()
+    .required('Password is required!')
+});
+
+export const ErrorHandling = ({ field, form: { touched, errors }, ...props }) => (
+  <TextField
+    {...field}
+    {...props}
+    error={touched[field.name] && !!errors[field.name]}
+    helperText={touched[field.name] && errors[field.name]}
+    fullWidth
+    size="small"
+    margin="dense"
+  />
+);
