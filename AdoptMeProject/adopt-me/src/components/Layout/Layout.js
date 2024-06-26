66884d40875d18cb3c4a5364040ecb73f0c290e2
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-26 18:40:48 +0300

    feat: creating a Layout component to structure the page with Material UI

---
+++ AdoptMeProject/adopt-me/src/components/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Box, Grid } from '@mui/material';
+
+import { Header } from '../Header/Header';
+import { Routing } from '../../routes/Routing';
+import { Footer } from '../Footer/Footer';
+
+export const Layout = () => {
+  return (
+    <Box
+      sx={{
+        backgroundImage: 'url(images/14710.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-between"
+        display="flex"
+        minHeight="100vh"
+      >
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item>
+          <Routing />
+        </Grid>
+        <Grid item>
+          <Footer />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
