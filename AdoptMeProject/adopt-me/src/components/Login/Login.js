cf816da37f4dd15642b1c8927b15d1d0e1576c69
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-26 18:28:50 +0300

    refactor: refactor Login component, styles and validations

--- AdoptMeProject/adopt-me/src/components/Login/Login.js
+++ AdoptMeProject/adopt-me/src/components/Login/Login.js
@@ -1,34 +1,24 @@
-import { Formik, Form, Field } from 'formik';
-import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import { Card, Button, Typography } from '@mui/material';
 
-import { Grid, TextField, Typography, Button } from '@mui/material';
-
-import { AuthContext } from '../../contexts/AuthContext';
-import { loginSchema } from './validations';
 import { styles } from './styles';
+import { loginSchema, ErrorHandling } from './validations';
+import { AuthContext } from '../../contexts/AuthContext';
+
 
 const initialValues = {
-  email: '',
-  password: ''
+  email: "",
+  password: ""
 }
 
-const ErrorHandling = ({ field, form: { touched, errors }, ...props }) => (
-  <TextField
-    {...field}
-    {...props}
-    error={touched[field.name] && !!errors[field.name]}
-    helperText={touched[field.name] && errors[field.name]}
-    fullWidth
-    margin='dense'
-  />
-);
 export const Login = () => {
   const { onLoginSubmit } = useContext(AuthContext);
 
   return (
-    <Grid style={styles.form}>
-      <Typography style={styles.header} variant='h1' >
+    <Card style={styles.form}>
+      <Typography style={styles.header} variant="h1" >
         Adopt ME
       </Typography>
       <Formik
@@ -41,21 +31,21 @@ export const Login = () => {
         {() => (
           <Form  >
             <Field
-              name='email'
-              type='email'
-              label='Email'
+              name="email"
+              type="email"
+              label="Email"
               component={ErrorHandling}
             />
             <Field
-              name='password'
-              type='password'
-              label='Password'
+              name="password"
+              type="password"
+              label="Password"
               component={ErrorHandling}
             />
             <Button
-              type='submit'
-              variant='contained'
-              color='info'
+              type="submit"
+              variant="contained"
+              color="info"
               fullWidth
               sx={styles.button}
             >
@@ -64,9 +54,9 @@ export const Login = () => {
           </Form>
         )}
       </Formik>
-      <Typography variant='overline'>
-        Don't have account? <Link to='/register'>Register</Link>
+      <Typography variant="overline">
+        Don't have account? <Link to="/register">Register</Link>
       </Typography>
-    </Grid>
+    </Card>
   );
 };
