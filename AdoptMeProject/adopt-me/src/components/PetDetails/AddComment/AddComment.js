8f4d8365ed518b82e492650afa9374596d719bff
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-27 18:09:52 +0300

    feat: add MaterialUI to the PetDetails component

--- AdoptMeProject/adopt-me/src/components/PetDetails/AddComment/AddComment.js
+++ AdoptMeProject/adopt-me/src/components/PetDetails/AddComment/AddComment.js
@@ -1,20 +1,38 @@
 import { useForm } from '../../../hooks/useForm';
 
+import { Button, Box, TextField } from '@mui/material';
+
 export const AddComment = ({
     onCommentSubmit,
 }) => {
     const { values, changeHandler, onSubmit } = useForm({
-        comment: ''
+        comment: ""
     }, onCommentSubmit);
 
     return (
-        //  {/* <!-- Add Comment ( Only htmlFor logged-in users, which is not creators of the current pet ) --> */}
-        <article className="create-comment">
-            <label>Add new comment:</label>
-            <form className="form" onSubmit={onSubmit}>
-                <textarea name="comment" placeholder="Comment..." value={values.comment} onChange={changeHandler}></textarea>
-                <input className="btn-submit" type="submit" value="Add Comment" />
-            </form>
-        </article>
+        <Box>
+            <Box onSubmit={onSubmit}>
+                <TextField
+                    labrl="Comment"
+                    name="comment"
+                    placeholder="Add new comment..."
+                    multiline
+                    rows={4}
+                    value={values.comment}
+                    onChange={changeHandler}
+                    variant="outlined"
+                    fullWidth
+                    sx={{ mt: 2, mb: 3 }}
+                >
+                </TextField>
+            </Box>
+            <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+            >
+                Add Comment
+            </Button>
+        </Box>
     );
 };
