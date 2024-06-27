7b91fe9d84f20c969c66290a57283f59a1c57178
Dani Simeonova <dani.simeonova@tbs.tech>
2024-06-27 18:08:21 +0300

    feat: add MaterialUI to the Footer component

--- AdoptMeProject/adopt-me/src/components/Footer/Footer.js
+++ AdoptMeProject/adopt-me/src/components/Footer/Footer.js
@@ -1,27 +1,70 @@
+import * as React from 'react';
+
+import { Facebook, LinkedIn } from '@mui/icons-material';
+import { Box, Container, Grid, Link, Typography } from '@mui/material';
+
 export const Footer = () => {
   return (
-    <footer>
-      <div className="about">
-        <h3>About Creator</h3>
-        <ul className="about-creator">
-          <li> <img src="/images/locationLogo.png" /> Sofia, Bulgaria</li>
-          <li> <img src="/images/mailLogo.png" /> danisimeonova147@gmail.com </li>
-          <li> <img src="/images/callLogo.png" /> +359895556107</li>
-        </ul>
-        <h3>Social networks</h3>
-        <ul className="social-networks">
-          <li>
-            <a href="https://www.facebook.com/dani.simeonova.589/">
-              <img src="/images/facebookLogo.png" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/dani-simeonova-945181279/">
-              <img src="/images/linkedInLogo.png" />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+    <Box
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+        p: 6,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              About Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Web application for sharing animals for adoption.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Sofia, Bulgaria
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: danisimeonova147@gmail.com
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +359895556107
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link
+              href="https://www.facebook.com/dani.simeonova.589/"
+              color="#2565ae"
+            >
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/dani-simeonova-945181279/"
+              color="#2565ae"
+            >
+              <LinkedIn />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="">
+              Adopt ME
+            </Link>
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   );
 };
