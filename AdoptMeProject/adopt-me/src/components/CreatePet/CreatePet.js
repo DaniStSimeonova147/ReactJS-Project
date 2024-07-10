81764bd38a5a48941873c0709d39a252aaaa6c7a
Dani Simeonova <dani.simeonova@tbs.tech>
2024-07-10 17:07:19 +0300

    refactor: unification of logic from CreatePet and EditPet to the PetForm component

--- AdoptMeProject/adopt-me/src/components/CreatePet/CreatePet.js
+++ AdoptMeProject/adopt-me/src/components/CreatePet/CreatePet.js
@@ -1,40 +1,5 @@
-import { usePetContext } from '../../contexts/PetContext';
-
-import { Formik } from "formik";
-import { Card, Container, Typography } from '@mui/material';
-
-import { petSchema } from '../PetForm/validations';
 import { PetForm } from '../PetForm/PetForm';
-import { styles } from '../Login/styles';
 
-const initialValues = {
-  name: "",
-  type: "",
-  age: "",
-  description: "",
-  imageUrl: "",
-  location: "",
-  contact: "",
-};
 export const CreatePet = () => {
-  const { onCreatePetSubmit } = usePetContext();
-
-  return (
-    <Container component="main" maxWidth="md" margin="auto">
-      <Card style={styles.form}>
-        <Typography style={styles.header} variant="h1">
-          Add pet
-        </Typography>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={petSchema}
-          onSubmit={(values) => {
-            onCreatePetSubmit(values);
-          }}
-        >
-          <PetForm buttonType="Add Pet"></PetForm>
-        </Formik>
-      </Card>
-    </Container>
-  );
+  return <PetForm card="create"></PetForm>
 };
--- AdoptMeProject/adopt-me/src/components/EditPet/EditPet.js
+++ AdoptMeProject/adopt-me/src/components/EditPet/EditPet.js
@@ -1,47 +1,5 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-
-import { Formik } from 'formik';
-import { Card, Container, Typography } from '@mui/material';
-
-import { usePetContext } from '../../contexts/PetContext';
-import { useService } from '../../hooks/useService';
-import { petServiceFactory } from '../../services/petService';
-import { petSchema } from '../PetForm/validations';
 import { PetForm } from '../PetForm/PetForm';
-import { styles } from '../Login/styles';
 
 export const EditPet = () => {
-    const { onPetEditSubmit } = usePetContext();
-    const { petId } = useParams();
-    const petService = useService(petServiceFactory);
-
-    const [petData, setPetData] = useState();
-
-    useEffect(() => {
-        petService.getOne(petId)
-            .then((result) => {
-                setPetData(result);
-            });
-    }, [petId]);
-
-    return (
-        <Container component="main" maxWidth="md" margin="auto">
-            <Card style={styles.form}>
-                <Typography style={styles.header} variant="h1">
-                    Edit pet
-                </Typography>
-                <Formik
-                    initialValues={petData || { name: '', type: '', age: '', description: '', imageUrl: '', location: '', contact: '' }}
-                    validationSchema={petSchema}
-                    onSubmit={(values) => {
-                        onPetEditSubmit(values);
-                    }}
-                    enableReinitialize
-                >
-                    <PetForm buttonType="Edit Pet"></PetForm>
-                </Formik>
-            </Card >
-        </Container>
-    );
+    return <PetForm card="edit"></PetForm>
 };
