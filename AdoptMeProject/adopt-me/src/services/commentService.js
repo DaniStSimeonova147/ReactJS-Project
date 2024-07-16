0f4a524e94fd9ba9b8627ebd12078eddd4635ff9
Dani Simeonova <dani.simeonova@tbs.tech>
2024-07-16 16:11:04 +0300

    fix: add token to the commentService

--- AdoptMeProject/adopt-me/src/services/commentService.js
+++ AdoptMeProject/adopt-me/src/services/commentService.js
@@ -5,20 +5,27 @@ const baseUrl = process.env.NODE_ENV === 'development'
     : 'http://localhost:3030';  // TODO: Add server url when deployed
 const url = `${baseUrl}/data/comments`;
 
-const request = requestFactory();
+export const commentServiceFactory = (token) => {
+    const request = requestFactory(token);
 
-export const getAll = async (petId) => {
-    const searchQuery = encodeURIComponent(`petId="${petId}"`);
-    const relationQuery = encodeURIComponent(`author=_ownerId:users`);
+    const getAll = async (petId) => {
+        const searchQuery = encodeURIComponent(`petId="${petId}"`);
+        const relationQuery = encodeURIComponent(`author=_ownerId:users`);
 
 
-    const result = await request.get(`${url}?where=${searchQuery}&load=${relationQuery}`);
-    const comments = Object.values(result);
-    return comments;
-};
+        const result = await request.get(`${url}?where=${searchQuery}&load=${relationQuery}`);
+        const comments = Object.values(result);
+        return comments;
+    };
 
-export const create = async (petId, comment) => {
-    const result = await request.post(url, { petId, comment });
+    const create = async (petId, comment) => {
+        const result = await request.post(url, { petId, comment }, token);
 
-    return result;
+        return result;
+    };
+
+    return {
+        getAll,
+        create,
+    };
 };
