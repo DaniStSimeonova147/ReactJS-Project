d9d2b3e7c3417b6af4b1b6faff50576daed9fd96
Dani Simeonova <dani.simeonova@tbs.tech>
2024-09-16 16:34:32 +0300

    refactor: refactor form validation and error handling

--- AdoptMeProject/adopt-me/src/components/Register/validations.js
+++ AdoptMeProject/adopt-me/src/components/Register/validations.js
@@ -5,23 +5,10 @@ export const registerSchema = Yup.object({
         .email('Invalid email format!')
         .required('Email is required!'),
     password: Yup.string()
-        .test('password-strength', function (value) {
-            const errors = [];
-            if (!value) {
-                errors.push('Password is required!');
-            } else {
-                if (value.length < 6) {
-                    errors.push('Password must be more than 6 symbols!');
-                }
-                if (!/[a-z]/.test(value)) {
-                    errors.push('Must contain one lowercase!');
-                }
-                if (!/[A-Z]/.test(value)) {
-                    errors.push('Must contain one uppercase!');
-                }
-            }
-            return errors.length > 0 ? this.createError({ message: errors.join('\n ') }) : true;
-        }),
+        .required('Password is required!')
+        .min(6, 'Contain at least 6 symbols!')
+        .matches(/[a-z]/, "At least one lowercase!")
+        .matches(/[A-Z]/, "At least one uppercase!"),
     confirmPassword: Yup.string()
         .required('Repeat password is required!')
         .oneOf([Yup.ref('password')], 'Passwords must match!')
